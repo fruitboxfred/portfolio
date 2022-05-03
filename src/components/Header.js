51699c9e28e2350e1a8399fcf381be3ca90ffe8b
fruitboxfred <allanbradicich@gmail.com>
2022-05-03 11:01:51 +0930

    made changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,12 @@ import profile from '../assets/profile.png';
 const Header = () => {
     return (
         <section className='header'>
-            <img src={profile} alt="John Doe" />
+            <img src={profile} alt="Al Bradicich" />
 
             <div className='header__content'>
-                <h1>Hi, I'm John Doe</h1>
-                <p>Blockchain Developer</p>
-                <button className='button'>Get In Touch</button>
+                <h1>Hi, I'm Al</h1>
+                <p> Jnr Web3 Developer</p>
+                <a href="mailto:allanbradicich@gmail.com" className='button'>Get In Touch</a>
             </div>
         </section>
     );
