7e34dea831c6d8b68b3477a7de492df64ec6d6b8
fruitboxfred <allanbradicich@gmail.com>
2022-05-03 17:49:02 +0930

    updating portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
                 Hi my name is Allan Bradicich and I live in Australia.
                 I started my crypto journey around 18 months ago. I am a qualified chef by trade and at the age of 50 I wanted a career change. I was facinated with blockchain and the deeper I dove the more I consumed my time in learning about web3 and the unlimitless potentials it presents.
                 Living in Australia I found that no university or training organization here, offered any courses in blockchain development so I embarked on a self taught journey, and considering I had no prior experience in coding what so ever
-                I threw myself in the deep end and have been learning ever since.  
+                I threw myself in the deep end and have been learning and developing ever since. Thank you for viewing my portfolio and I look forward to working with you soon.  
             </p>
         </section>
     );
